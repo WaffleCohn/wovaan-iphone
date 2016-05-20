11eeb484aeffd4bbe682a01ec2da8ed3de34725a
WaffleCohn <Ari@Aris-MacBook-Air.local>
2016-05-20 12:34:44 -0500

    Timer runs

--- Wovaan/Wovaan/TimerViewController.swift
+++ Wovaan/Wovaan/TimerViewController.swift
@@ -8,16 +8,107 @@
 
 import UIKit
 
-class TimerViewController: UIViewController {
+class TimerViewController: UIViewController
+{
+    @IBOutlet weak var timeLabel: UILabel!
 
-    override func viewDidLoad() {
+    var timer = NSTimer()
+    
+    let interval = 0.01
+    
+    var count = 0.0
+    
+    var counting = false
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let tap = UITapGestureRecognizer(target: self, action: "tap:")
+        self.view.addGestureRecognizer(tap)
+        
+        //play()
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+    }
+    
+    func tap(sender: UITapGestureRecognizer)
+    {
+        
+        if (counting)
+        {
+            stop()
+        }
+        else
+        {
+            play()
+        }
+        
+    }
+    
+    func play()
+    {
+        
+        counting = true
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(interval, target: self, selector: Selector("updateTime"), userInfo: nil, repeats: true)
+        
+    }
+    
+    func stop()
+    {
+        
+        counting = false
+        
+        timer.invalidate()
+        
+    }
+    
+    func updateTime()
+    {
+        count += interval
+        
+        timeLabel.text = formatTime(count)
+    }
+    
+    func formatTime(var x:Double) -> String
+    {
+        var hrs = 0
+        var min = 0
+        var sec = 0
+        var ms = 0
+        
+        if (x / 3600 >= 1)
+        {
+            hrs = Int(x/3600)
+            x -= Double(hrs*3600)
+        }
+        
+        if (x / 60 >= 1)
+        {
+            min = Int(Int(x)/60)
+            x -= Double(min*60)
+        }
+        
+        sec = Int(x)
+        x -= Double(sec)
+        ms = Int(x*100)
+        
+        if (hrs > 0)
+        {
+            return String(format: "\(hrs):%02d:%02d.%02d", arguments: [min, sec, ms])
+        }
+        
+        if (min > 0)
+        {
+            return String(format: "\(min):%02d.%02d", arguments: [sec, ms])
+        }
+        
+        return String(format: "\(sec).%02d", arguments: [ms])
     }
 
 
