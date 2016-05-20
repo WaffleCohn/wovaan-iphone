a79f6ccc91f2e697b5552ebe5af2ec8a8bf52d85
WaffleCohn <Ari@Aris-MacBook-Air.local>
2016-05-19 19:21:41 -0500

    Initialization

---
+++ Wovaan/Wovaan/TimeCell.swift
@@ -0,0 +1,24 @@
+//
+//  TimeCell.swift
+//  Wovaan
+//
+//  Created by Ari Cohn on 5/19/16.
+//  Copyright © 2016 Ari Cohn. All rights reserved.
+//
+
+import UIKit
+
+class TimeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Wovaan/Wovaan/TimerViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Wovaan
+//
+//  Created by Ari Cohn on 5/19/16.
+//  Copyright © 2016 Ari Cohn. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
